7f8eed65f6d8e08b5193c182d75bd2384312d6ba
Jose Carrero <josercl@gmail.com>
2023-12-05 11:43:06 -0400

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
+    fun fixNumbers(s: String): String {
+        if (s.first().isDigit() && s.last().isDigit()) {
+            return s
+        }
+
+        return s.replace("one", "o1e")
+            .replace("two","t2o")
+            .replace("three", "t3e")
+            .replace("four", "f4r")
+            .replace("five", "f5e")
+            .replace("six", "s6x")
+            .replace("seven", "s7n")
+            .replace("eight", "e8t")
+            .replace("nine", "n9e")
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            line.filter(Char::isDigit)
+        }.map {
+            "${it.first()}${it.last()}"
+        }.sumOf { it.toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return part1(input.map(::fixNumbers))
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
